7bdcdb93a09c2bae2613edce911075615f83f3ed
yoongeemin <yoongeemin@gmail.com>
2016-03-24 10:56:49 -0400

    Update rundev.js

--- gulp/rundev.js
+++ gulp/rundev.js
@@ -2,7 +2,7 @@ const bluebird = require("bluebird");
 const path = require("path");
 const nodemon = require("gulp-nodemon");
 const fs = require("fs");
-const logger = require("../logger");
+const LOGGER = require("../logger");
 
 function getServerManifest(filename) {
     return bluebird.promisify(fs.readFile)(filename)
@@ -11,7 +11,7 @@ function getServerManifest(filename) {
             if (manifest.server) {
                 return manifest.server.js;
             }
-            logger.error(`${filename} does not contain server manifest`);
+            LOGGER.error(`${filename} does not contain server manifest`);
             throw new Error(`${filename} does not contain server manifest`);
         })
         .catch(function() {
@@ -25,24 +25,24 @@ module.exports = function() {
     const filename = path.resolve(__dirname, "..", "public", "manifest.json");
 
     const timeout = setTimeout(() => {
-        logger.error(`Unable to load ${filename}`);
+        LOGGER.error(`Unable to load ${filename}`);
         throw new Error(`Unable to load ${filename}`);
     }, 60 * 1000);
 
     getServerManifest(filename).then(function(server) {
         clearTimeout(timeout);
-        logger.info(`Successfullly loaded ${filename}`);
+        LOGGER.info(`Successfullly loaded ${filename}`);
         nodemon({
             script: path.resolve(__dirname, "..", "public", server),
             watch: [path.resolve(__dirname, "..", "server")],
             env: {
                 "NODE_ENV": "development",
-                "PORT": 8000,
+                "PORT": 8080,
             },
             ext: "js hjs",
         })
         .on("restart", () => {
-            logger.info("Nodemon restarting server");
+            LOGGER.info("Nodemon restarting server");
         });
     });
 };
