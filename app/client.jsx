1485407873d388d9babd034a5692f84efc412607
yoongeemin <yoongeemin@gmail.com>
2016-03-28 16:47:57 -0400

    Update client.jsx

--- app/client.jsx
+++ app/client.jsx
@@ -3,45 +3,19 @@ import "./styles/base/index";
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import { syncHistoryWithStore } from "react-router-redux";
-import { configureStore } from "./lib/redux";
-import { App, SignUp } from "./containers/index";
-import { Authentication, DashBoard } from "./components/index";
 import reducers from "./reducers/index";
+import { configureStore } from "./lib/redux";
+import configureRoutes from "./config/routes";
 
 const store = configureStore(reducers);
+const routes = configureRoutes(store);
 const history = syncHistoryWithStore(browserHistory, store);
-
-const authentication = (nextState, replace) => {
-    const authenticated = !store.getState().userReducer.profile.isEmpty();
-
-    console.log(store.getState());
-
-    if (!authenticated) {
-        //replace({
-        //    pathname: "/signin",
-        //    state: { nextPathname: "/dashboard" },
-        //});
-        console.log("***no auth!");
-    }
-    else {
-        //replace("/dashboard");
-        console.log("***auth!");
-    }
-};
 
 ReactDOM.render(
     (
         <Provider store={store}>
-            <Router history={history}>
-                <Route path="/" component={App}>
-                    <IndexRoute onEnter={authentication} />
-                    <Route path="/signin" component={Authentication} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/dashboard" component={DashBoard} />
-                </Route>
-            </Router>
+            <Router history={history} routes={routes} />
         </Provider>
     ),
     document.getElementById("main")
