879b4d4c235135b7e3458cc48c2ccc1110e21ef0
yoongeemin <yoongeemin@gmail.com>
2016-04-06 16:43:01 -0400

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,4 @@
 const Router = require("koa-router");
-//const path = require("path");
-//const loadManifest = require("../lib/promises/manifest").loadManifest;
 const authenticationControllers = require("../controllers/authentication");
 
 const server = require("../../public/assets/server");
@@ -11,10 +9,16 @@ module.exports = (app) => {
     const router = new Router();
 
     // Local authentication
-    router.post(`${API_PREFIX}/signin`, authenticationControllers.signIn);
-    router.post(`${API_PREFIX}/signup`, authenticationControllers.signUp);
-    router.get(`${API_PREFIX}/signout`, authenticationControllers.signOut);
-    router.get("/activate/:uid/:token", authenticationControllers.activate);
+    router.post(`${API_PREFIX}/authenticate`,        authenticationControllers.authenticate);
+    router.post(`${API_PREFIX}/signin`,              authenticationControllers.signIn);
+    router.post(`${API_PREFIX}/signup`,              authenticationControllers.signUp);
+    router.get(`${API_PREFIX}/signout`,              authenticationControllers.signOut);
+    router.get(`${API_PREFIX}/activate/:uid/:token`, authenticationControllers.activate);
+
+	// Application API
+    router.get(`${API_PREFIX}/quotes`, appControllers.getQuotes);
+    
+    // Server side rendering
     router.get("*", server.render);
 
     app.use(router.routes());
