3e1b55dcfeab7f851215ce3f72c9c8beac35d3d7
YG <yoongeemin@gmail.com>
2016-04-18 17:25:44 -0400

    Update prod.js

--- webpack/prod.js
+++ webpack/prod.js
@@ -4,15 +4,15 @@ const webpack = require("webpack");
 const Configurator = require("webpack-config");
 
 module.exports = new Configurator()
-.extend(path.resolve(__dirname, "base"))
-.merge({
-    debug: false,
-
-    plugins: [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            sourceMap: false,
-        }),
-    ],
-});
+    .extend(path.resolve(__dirname, "base"))
+    .merge({
+        debug: false,
+    
+        plugins: [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                sourceMap: false,
+            }),
+        ],
+    });
