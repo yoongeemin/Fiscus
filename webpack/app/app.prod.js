cf53d6f28475f29fac979d9a6d59863782d6569c
YG <yoongeemin@gmail.com>
2016-04-18 17:21:44 -0400

    Update app.prod.js

--- webpack/app/app.prod.js
+++ webpack/app/app.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const Configurator = require("webpack-config");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const env = require("../../env/prod.json");
 
 module.exports = new Configurator()
 .extend(path.resolve(__dirname, "app.base"))
@@ -12,6 +13,14 @@ module.exports = new Configurator()
 
     plugins: [
         new ExtractTextPlugin("[name].css"),
+        new webpack.DefinePlugin({
+            __CLIENT__: true,
+            process.env: {
+                NODE_ENV: JSON.stringify(env.NODE_ENV),
+                HOSTNAME: JSON.stringify(env.HOSTNAME),
+                PORT JSON.stringify(env.PORT),
+            },
+        }),
     ],
 
     module: {
