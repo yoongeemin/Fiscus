c64bac0eaf79c1270eac7be010c94924e5ea3634
YG <yoongeemin@gmail.com>
2016-04-18 17:23:31 -0400

    Update server.dev.js

--- webpack/server/server.dev.js
+++ webpack/server/server.dev.js
@@ -2,6 +2,6 @@ const path = require("path");
 const Configurator = require("webpack-config");
 
 module.exports = new Configurator()
-.extend(path.resolve(__dirname, "server.base"))
-.extend(path.resolve(__dirname, "..", "dev"))
-.merge({ });
+    .extend(path.resolve(__dirname, "server.base"))
+    .extend(path.resolve(__dirname, "..", "dev"))
+    .merge({ });
