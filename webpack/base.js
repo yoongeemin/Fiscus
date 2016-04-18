77a8a2d6dbff28c19f94a83f0d268504750701ea
YG <yoongeemin@gmail.com>
2016-04-18 17:23:59 -0400

    Update base.js

--- webpack/base.js
+++ webpack/base.js
@@ -5,46 +5,47 @@ const Configurator = require("webpack-config");
 
 const nodePath = path.resolve(__dirname, "..", "node_modules");
 
-module.exports = new Configurator().merge({
-    output: {
-        path: path.resolve(__dirname, "..", "public", "assets"),
-        publicPath: "/assets/",
-    },
-
-    resolve: {
-        extensions: ["", ".js", ".jsx"],
-
-        alias: {
-            "bootstrap-css": `${nodePath}/bootstrap/dist/css/bootstrap.min.css`,
-            "bootstrap-theme-css": `${nodePath}/bootstrap/dist/css/bootstrap-theme.min.css`,
-            "bootstrap-js": `${nodePath}/bootstrap/dist/js/bootstrap.min.js`,
-            "jquery": `${nodePath}/jquery/dist/jquery.min.js`,
-            "jquery-ui": `${nodePath}/jquery-ui/themes/smoothness/jquery-ui.min.css`,
+module.exports = new Configurator()
+    .merge({
+        output: {
+            path: path.resolve(__dirname, "..", "public", "assets"),
+            publicPath: "/assets/",
         },
-    },
-
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(true),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            _: "lodash",
-        }),
-    ],
-
-    module: {
-        loaders: [
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)$/,
-                loader: "url-loader",
-                query: {
-                    name: "[hash].[ext]",
-                    limit: 10000,
-                }
+    
+        resolve: {
+            extensions: ["", ".js", ".jsx"],
+    
+            alias: {
+                "bootstrap-css": `${nodePath}/bootstrap/dist/css/bootstrap.min.css`,
+                "bootstrap-theme-css": `${nodePath}/bootstrap/dist/css/bootstrap-theme.min.css`,
+                "bootstrap-js": `${nodePath}/bootstrap/dist/js/bootstrap.min.js`,
+                "jquery": `${nodePath}/jquery/dist/jquery.min.js`,
+                "jquery-ui": `${nodePath}/jquery-ui/themes/smoothness/jquery-ui.min.css`,
             },
-            {
-                include: /\.json$/,
-                loaders: ["json-loader"],
-            },
+        },
+    
+        plugins: [
+            new webpack.optimize.OccurenceOrderPlugin(true),
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                _: "lodash",
+            }),
         ],
-    },
-});
+    
+        module: {
+            loaders: [
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)$/,
+                    loader: "url-loader",
+                    query: {
+                        name: "[hash].[ext]",
+                        limit: 10000,
+                    }
+                },
+                {
+                    include: /\.json$/,
+                    loaders: ["json-loader"],
+                },
+            ],
+        },
+    });
