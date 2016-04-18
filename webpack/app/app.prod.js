29368a332a7a37a851ff144aaa6e33fd3aba8fb8
YG <yoongeemin@gmail.com>
2016-04-18 17:22:31 -0400

    Update app.prod.js

--- webpack/app/app.prod.js
+++ webpack/app/app.prod.js
@@ -4,32 +4,32 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const env = require("../../env/prod.json");
 
 module.exports = new Configurator()
-.extend(path.resolve(__dirname, "app.base"))
-.extend(path.resolve(__dirname, "..", "prod"))
-.merge({
-    output: {
-        filename: "[name].js",
-    },
-
-    plugins: [
-        new ExtractTextPlugin("[name].css"),
-        new webpack.DefinePlugin({
-            __CLIENT__: true,
-            process.env: {
-                NODE_ENV: JSON.stringify(env.NODE_ENV),
-                HOSTNAME: JSON.stringify(env.HOSTNAME),
-                PORT JSON.stringify(env.PORT),
-            },
-        }),
-    ],
-
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
+    .extend(path.resolve(__dirname, "app.base"))
+    .extend(path.resolve(__dirname, "..", "prod"))
+    .merge({
+        output: {
+            filename: "[name].js",
+        },
+    
+        plugins: [
+            new ExtractTextPlugin("[name].css"),
+            new webpack.DefinePlugin({
+                __CLIENT__: true,
+                process.env: {
+                    NODE_ENV: JSON.stringify(env.NODE_ENV),
+                    HOSTNAME: JSON.stringify(env.HOSTNAME),
+                    PORT JSON.stringify(env.PORT),
+                },
+            }),
         ],
-    },
-});
+    
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },
+            ],
+        },
+    });
