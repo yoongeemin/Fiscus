2fef8a03887ca1d4633881e192ee6e268df5603d
YG <yoongeemin@gmail.com>
2016-04-18 17:22:59 -0400

    Update app.base.js

--- webpack/app/app.base.js
+++ webpack/app/app.base.js
@@ -3,17 +3,18 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const Configurator = require("webpack-config");
 
-module.exports = new Configurator().merge({
-    entry: {
-        app: [path.resolve(__dirname, "..", "..", "app", "client.jsx")],
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.(css|scss)$/,
-                loader: ExtractTextPlugin.extract("style", "css!sass"),
-            },
-        ],
-    },
-});
+module.exports = new Configurator()
+    .merge({
+        entry: {
+            app: [path.resolve(__dirname, "..", "..", "app", "client.jsx")],
+        },
+    
+        module: {
+            loaders: [
+                {
+                    test: /\.(css|scss)$/,
+                    loader: ExtractTextPlugin.extract("style", "css!sass"),
+                },
+            ],
+        },
+    });
