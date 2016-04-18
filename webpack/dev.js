eaa7935cb3571e0fe8c13dddde4f2f40822d450f
YG <yoongeemin@gmail.com>
2016-04-18 17:25:36 -0400

    Update dev.js

--- webpack/dev.js
+++ webpack/dev.js
@@ -3,19 +3,19 @@ const path = require("path");
 const Configurator = require("webpack-config");
 
 module.exports = new Configurator()
-.extend(path.resolve(__dirname, "base"))
-.merge({
-    eslint: {
-        configFile: path.resolve(__dirname, "..", ".eslintrc.react"),
-    },
-
-    module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-            },
-        ],
-    },
-});
+    .extend(path.resolve(__dirname, "base"))
+    .merge({
+        eslint: {
+            configFile: path.resolve(__dirname, "..", ".eslintrc.react"),
+        },
+    
+        module: {
+            preLoaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                },
+            ],
+        },
+    });
