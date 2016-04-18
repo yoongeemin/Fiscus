d04a119e55884b6aa1ef69a46df16888a99309d0
YG <yoongeemin@gmail.com>
2016-04-18 17:22:42 -0400

    Update app.dev.js

--- webpack/app/app.dev.js
+++ webpack/app/app.dev.js
@@ -6,55 +6,55 @@ const AssetsPlugin = require("assets-webpack-plugin");
 const env = require("../../env/dev.json");
 
 module.exports = new Configurator()
-.extend(path.resolve(__dirname, "app.base"))
-.extend(path.resolve(__dirname, "..", "dev"))
-.merge({
-    debug: true,
-
-    devtool: "cheap-module-source-map",
-
-    // Add webpack hot reloading
-    entry: {
-        app: [
-            "webpack-hot-middleware/client",
-            "webpack/hot/dev-server",
-        ],
-    },
-
-    output: {
-        filename: "[name].[hash].js",
-    },
-
-    plugins: [
-        new ExtractTextPlugin("[name].[hash].css"),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new AssetsPlugin({
-            fullPath: false,
-            path: path.resolve(__dirname, "..", "..", "public"),
-            filename: "manifest.json",
-        }),
-        new webpack.DefinePlugin({
-            __CLIENT__: true,
-            process.env: {
-                NODE_ENV: JSON.stringify(env.NODE_ENV),
-                HOSTNAME: JSON.stringify(env.HOSTNAME),
-                PORT JSON.stringify(env.PORT),
-            },
-        }),
-    ],
-
-
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react-hmre"],
+    .extend(path.resolve(__dirname, "app.base"))
+    .extend(path.resolve(__dirname, "..", "dev"))
+    .merge({
+        debug: true,
+    
+        devtool: "cheap-module-source-map",
+    
+        // Add webpack hot reloading
+        entry: {
+            app: [
+                "webpack-hot-middleware/client",
+                "webpack/hot/dev-server",
+            ],
+        },
+    
+        output: {
+            filename: "[name].[hash].js",
+        },
+    
+        plugins: [
+            new ExtractTextPlugin("[name].[hash].css"),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin(),
+            new AssetsPlugin({
+                fullPath: false,
+                path: path.resolve(__dirname, "..", "..", "public"),
+                filename: "manifest.json",
+            }),
+            new webpack.DefinePlugin({
+                __CLIENT__: true,
+                process.env: {
+                    NODE_ENV: JSON.stringify(env.NODE_ENV),
+                    HOSTNAME: JSON.stringify(env.HOSTNAME),
+                    PORT JSON.stringify(env.PORT),
                 },
-            },
+            }),
         ],
-    },
-});
+    
+    
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    query: {
+                        presets: ["react-hmre"],
+                    },
+                },
+            ],
+        },
+    });
