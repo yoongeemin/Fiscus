53457bb02f912dc46ab156734472975e34cd914b
YG <yoongeemin@gmail.com>
2016-04-18 17:23:16 -0400

    Update server.base.js

--- webpack/server/server.base.js
+++ webpack/server/server.base.js
@@ -2,36 +2,37 @@ const path = require("path");
 const webpack = require("webpack");
 const Configurator = require("webpack-config");
 
-module.exports = new Configurator().merge({
-    target: "node",
-
-    node: {
-        __dirname: true,
-        __filename: true,
-    },
-
-    entry: {
-        server: [path.resolve(__dirname, "..", "..", "app", "server.jsx")],
-    },
-
-    output: {
-        filename: "[name].js",
-        libraryTarget: "commonjs2",
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            __CLIENT__: false,
-        }),
-    ],
-
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
+module.exports = new Configurator()
+    .merge({
+        target: "node",
+    
+        node: {
+            __dirname: true,
+            __filename: true,
+        },
+    
+        entry: {
+            server: [path.resolve(__dirname, "..", "..", "app", "server.jsx")],
+        },
+    
+        output: {
+            filename: "[name].js",
+            libraryTarget: "commonjs2",
+        },
+    
+        plugins: [
+            new webpack.DefinePlugin({
+                __CLIENT__: false,
+            }),
         ],
-    },
-});
+    
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },
+            ],
+        },
+    });
